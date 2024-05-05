38a81dd331ee6f8c82ed2e10d22f5f8981c0d98c
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-05-05 16:06:52 +0300

    2

--- js/lesson-4/lesson-4.js
+++ js/lesson-4/lesson-4.js
@@ -25,7 +25,7 @@ for (let i = 0; i < boxes; i++) {
   divContainer.appendChild(div);
 }
 
-section.append(divContainer)
+section.append(divContainer);
 
 // section.prepend(divContainer)
 // section.after(divContainer)
@@ -35,3 +35,58 @@ section.append(divContainer)
 // section.insertAdjacentHTML('beforeend', divContainer)
 
 // console.log(divContainer);
+
+//TODO:=========task-02=================
+
+const films = [
+  {
+    title: 'Tetris',
+    imgUrl: 'https://static.hdrezka.ac/i/2023/3/20/f509264b419fdmu53x38j.jpg',
+    id: 'film_1',
+  },
+  {
+    title: 'Avatar: The Way of Water',
+    imgUrl: 'https://static.hdrezka.ac/i/2022/12/22/tc5e6b8212683gn66r84s.jpg',
+    id: 'film_2',
+  },
+  {
+    title: 'Operation Fortune: Ruse de guerre',
+    imgUrl: 'https://static.hdrezka.ac/i/2022/2/11/s0d53f6cf0ae0tq29m85l.jpg',
+    id: 'film_3',
+  },
+  {
+    title: 'Babylon',
+    imgUrl: 'https://static.hdrezka.ac/i/2022/12/25/z330b47a82209ww99w55a.jpg',
+    id: 'film_4',
+  },
+  {
+    title: 'The Whale',
+    imgUrl: 'https://static.hdrezka.ac/i/2023/2/24/h23d8c65d734akd89q94c.jpg',
+    id: 'film_5',
+  },
+];
+
+// Приклад елементу списку
+{
+  /* <li id="${id}">
+     <img src="${imgUrl}" alt="${title}" />
+     <p>${title}</p>
+</li> */
+}
+
+const filmsList = document.querySelector('.films-list');
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      item =>
+        `<li id="${item.id}">
+     <img src="${item.imgUrl}" alt="${item.title}" />
+     <p>${item.title}</p>
+    </li> `
+    )
+    .join('');
+}
+
+// filmsList.innerHTML = createMarkup(films);
+filmsList.insertAdjacentHTML('beforeend', createMarkup(films));
